1359f6074313b44580b2194673fcc455373858b9
adjay27 <adityawhyjayaa@gmail.com>
2023-12-26 13:32:30 +0700

    add logo github

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -14,9 +14,10 @@ export function Landing() {
                 professional career. You can add more information on the about
                 page.
               </div>
+              <br />
 
               <div className="Buttons justify-start items-start gap-3 inline-flex">
-                <button className="ButtonPrimary px-6 py-2 bg-amber-400 rounded-lg justify-start items-start flex">
+                <button className="ButtonPrimary px-6 py-2 bg-blue-400 rounded-lg justify-start items-start flex">
                   <div className="Text text-zinc-800 text-lg font-medium font-['Roboto'] leading-[27px]">
                     Projects
                   </div>
@@ -94,6 +95,43 @@ export function Landing() {
               className="PexelsEllyFairytale38232071 w-[496px] h-[526px] rounded-r-3xl"
               src="https://via.placeholder.com/496x526"
             />
+          </div>
+
+          <div className=" w-[992px] h-[524px] shadow-md justify-start items-start inline-flex">
+            <div className="project-info rounded-l-3xl w-[496px] h-[524px] pl-[50.44px] pr-[38.88px] pt-[146.50px] pb-[145.50px] bg-custom-white flex-col justify-center items-center inline-flex ">
+              <div className="Frame3350 h-[232px] flex-col justify-start items-start gap-6 inline-flex">
+                <div className="Text self-stretch text-zinc-800 text-[40px] font-bold font-['Playfair Display'] leading-[60px]">
+                  Project Name 2
+                </div>
+                <div className="Text self-stretch text-zinc-500 text-lg font-normal font-['Nunito'] leading-[27px]">
+                  I created this personal project in order to show how to create
+                  an interface in Figma using a portfolio as an example.
+                </div>
+                <div className="BotOSecudRio px-6 py-2 rounded-3xl border border-zinc-800 justify-start items-start inline-flex">
+                  <button className="Text text-zinc-800 text-lg font-medium font-['Roboto'] leading-[27px]">
+                    View Project
+                  </button>
+                </div>
+              </div>
+            </div>
+            <img
+              className="PexelsEllyFairytale38232071 w-[496px] h-[526px] rounded-r-3xl"
+              src="https://via.placeholder.com/496x526"
+            />
+          </div>
+
+
+          <div>
+            <div  className="Text self-stretch text-zinc-800 text-[20px] font-bold font-['Playfair Display'] leading-[60px]">Contact Me</div>
+            <div className="grid grid-flow-col gap-8">
+            <div className="h-10 w-27 pr-2 rounded-lg bg-[#0D1117] flex justify-center">
+              <img className="invert h-auto w-auto m-1"
+                    src="./src/assets/iconmonstr-github-1.svg"
+                    />
+              <button className="h-10 w-auto text-white">Github</button>
+            </div>
+            <div className="h-10 w-10  flex justify-center">logo</div>
+            </div>
           </div>
         </div>
       </div>
